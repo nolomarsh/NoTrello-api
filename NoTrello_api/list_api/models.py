8663657a8212861c93a1ceb776bdbf952229af99
Kristian <kristianreyes7@icloud.com>
2021-08-20 07:45:53 -0700

    This database is connected to psql

--- NoTrello_api/list_api/models.py
+++ NoTrello_api/list_api/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class List(models.Model):
     title = models.CharField(max_length=155)
-    description: models.CharField(max_length=320)    
+    description = models.CharField(max_length=320)    
 
 class Card(models.Model):
-    name: models.CharField(max_length=155)
-    body: models.CharField(max_length=320)
-    labels: models.CharField(max_length=155)
-    image: models.URLField(max_length=320)
-    status: models.BooleanField()
+    name = models.CharField(max_length=155)
+    body = models.CharField(max_length=320)
+    labels = models.CharField(max_length=155)
+    image = models.URLField()
+    status = models.BooleanField()
     list = models.ForeignKey(List, on_delete=models.CASCADE, null=True,)
 
