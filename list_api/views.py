02b87cbe706eee2f26425f3d40ccc7fe42f75492
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-20 15:54:49 -0400

    added order_by('id') to views

--- list_api/views.py
+++ list_api/views.py
@@ -5,17 +5,17 @@ from .models import Card, List
 
 # Create your views here.
 class List_list(generics.ListCreateAPIView):
-    queryset = List.objects.all()
+    queryset = List.objects.all().order_by('id')
     serializer_class = ListSerializer
 
 class List_Detail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = List.objects.all()
+    queryset = List.objects.all().order_by('id')
     serializer_class = ListSerializer
+
 class CardList(generics.ListCreateAPIView):
-    queryset = Card.objects.all()
+    queryset = Card.objects.all().order_by('id')
     serializer_class = CardSerializer
 
 class CardDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Card.objects.all()
+    queryset = Card.objects.all().order_by('id')
     serializer_class = CardSerializer
-
