ff90ef5043d2cc7ac6ec31805037c780ac362c83
Kristian <kristianreyes7@icloud.com>
2021-08-23 08:11:36 -0700

    pw encryption

--- list_api/serializers.py
+++ list_api/serializers.py
@@ -18,18 +18,18 @@ class UserAccountSerializer(serializers.ModelSerializer):
         model = UserAccount
         fields = ('id', 'username', 'password')
 
-#hashes pw when they create an account
-def create(self, validated_data):
-    user = UserAccount.objects.create(
-        username=validated_data['username'],
-        password = make_password(validated_data['password'])
-    )
-    user.save()
-    return user
+    #hashes pw when they create an account
+    def create(self, validated_data):
+        user = UserAccount.objects.create(
+            username=validated_data['username'],
+            password = make_password(validated_data['password'])
+        )
+        user.save()
+        return user
 
-#this makes sure pw's are hashed
-def update(self, instance, validated_data):
-    user = UserAccount.objects.get(username=validated_data["username"])
-    user.password = make_password(validated_data["password"])
-    user.save()
-    return user
+    #this makes sure pw's are hashed
+    def update(self, instance, validated_data):
+        user = UserAccount.objects.get(username=validated_data["username"])
+        user.password = check_password(validated_data["password"])
+        user.save()
+        return user
