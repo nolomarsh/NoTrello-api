97124db310e3797e6b7497aead27296a8f81ff1a
Kristian <kristianreyes7@icloud.com>
2021-08-24 08:21:59 -0700

    Now sends a message

--- list_api/views.py
+++ list_api/views.py
@@ -39,9 +39,9 @@ def check_login(request):
             if check_password(password, user.password):
                 return JsonResponse({'id':user.id, 'username': user.username})
             else: 
-                return JsonResponse({'password': ['Error wrong password']}, status=500)
+                return JsonResponse({'status':'false', 'password': ['Error wrong password']}, status=500)
         else:
-            return JsonResponse({'username': ['Error no user with this username exists']}, status=500)
+            return JsonResponse({'status':'false', 'username': ['Error no user with this username exists']}, status=500)
 
 
 class List_list(generics.ListCreateAPIView):
